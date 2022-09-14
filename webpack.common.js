9d31a71ae8e2da24d53611c9988c41ae629b5d5b
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-09-14 18:09:26 +0900

    Fix : webpack 수정

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
@@ -8,55 +7,46 @@ webpack-dev-server 관련 코드를 제거하고 CleanWebpackPlugin 설치하
 CleanWebpackPlugin은 re-build될 때 기존의 결과물을 제거해준다. 
 */
 const config = {
-  entry: "./src/index.js",
+  entry: "./javascripts/index.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "build.js",
+    assetModuleFilename: "images/[name][ext][query]",
   },
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
         exclude: /node_modules/,
       },
       {
         test: /\.(sa|sc|c)ss$/,
         // use의 경우 오른쪽에서 왼쪽으로 읽음.
         // sass -> css -> MiniCssExtract
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
         // css-loader: css 파일을 import 가능, css 파일을 읽어줌
         // style-loader: 읽은 css 코드를 head 태그안에 넣어줌
         // MiniCssExtractPlugin은 style code를 한가지 css파일로 묶어줌
       },
       {
-        test: /\.(png|jpe?g|gif|svg|ico)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              // useRelativePath: true,
-              name: "images/[name].[ext]?[hash]",
-              // publicPath: "./dist/",
-              limit: 10000,
-            },
-          },
-        ],
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: "asset/resource",
       },
     ],
   },
 
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-    }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
+      template: path.resolve(__dirname, "./index.html"),
       inject: true,
       filename: path.resolve(__dirname, "dist/index.html"),
     }),
-    new CleanWebpackPlugin({ filename: "build.js" }),
+    new CleanWebpackPlugin({ filename: "build.js" }), // re-build될 때 기존의 결과물을 제거해준다
   ],
 };
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,8 @@ const path = require("path");
 const commonConfig = require("./webpack.common");
 const { merge } = require("webpack-merge");
 
-// 공통으로 설정한 config 모듈을 불러와서 merge 시켰다.
+// 공통으로 설정한 config 모듈을 불러와서 merge 시켰다.
+// webpack-dev-server의 빌드 결과물은 사실, 실제 파일로 빌드되진 않고 메모리에 저장된다.
 module.exports = merge(commonConfig, {
   // 개발모드
   mode: "development",
@@ -11,8 +12,8 @@ module.exports = merge(commonConfig, {
   // dist의 빌드한 내용을 서버로 띄우겠다.
 
   devServer: {
-    contentBase: path.resolve(__dirname, "./public"),
-    port: 9000,
+    static: path.resolve(__dirname, "./dist"),
+    port: 3000,
     // 빌드 대상 파일이 변경되면 자동으로 브라우저를 새로고침
     hot: true,
   },

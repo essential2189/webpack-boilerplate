6552ef3748ada729926426c586b0d74f73f05c2e
essential2189 <70889358+essential2189@users.noreply.github.com>
2022-10-14 19:40:07 +0900

    webpack 수정

--- webpack.common.js
+++ webpack.common.js
@@ -7,17 +7,26 @@ webpack-dev-server 관련 코드를 제거하고 CleanWebpackPlugin 설치하
 CleanWebpackPlugin은 re-build될 때 기존의 결과물을 제거해준다. 
 */
 const config = {
-  entry: "./javascripts/index.js",
+  entry: {
+    index: "./javascripts/index.js",
+    signin: "./javascripts/signin.js",
+    signup: "./javascripts/signup.js",
+    search: "./javascripts/search.js",
+  },
   output: {
+    publicPath: "",
     path: path.resolve(__dirname, "./dist"),
-    filename: "build.js",
-    assetModuleFilename: "images/[name][ext][query]",
+    assetModuleFilename: "[name][ext][query]",
   },
 
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.m?js$/,
         use: {
           loader: "babel-loader",
           options: { presets: ["@babel/preset-env"] },
@@ -27,15 +36,17 @@ const config = {
       {
         test: /\.(sa|sc|c)ss$/,
         // use의 경우 오른쪽에서 왼쪽으로 읽음.
-        // sass -> css -> MiniCssExtract
+        // sass -> css -> style-loader
         use: ["style-loader", "css-loader", "sass-loader"],
         // css-loader: css 파일을 import 가능, css 파일을 읽어줌
         // style-loader: 읽은 css 코드를 head 태그안에 넣어줌
-        // MiniCssExtractPlugin은 style code를 한가지 css파일로 묶어줌
       },
       {
         test: /\.(png|jpe?g|gif|svg|ico)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext][query]",
+        },
       },
     ],
   },
@@ -45,8 +56,31 @@ const config = {
       template: path.resolve(__dirname, "./index.html"),
       inject: true,
       filename: path.resolve(__dirname, "dist/index.html"),
+      chunks: ["index"],
     }),
-    new CleanWebpackPlugin({ filename: "build.js" }), // re-build될 때 기존의 결과물을 제거해준다
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./signin.html"),
+      inject: true,
+      filename: path.resolve(__dirname, "dist/signin.html"),
+      chunks: ["signin"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./signup.html"),
+      inject: true,
+      filename: path.resolve(__dirname, "dist/signup.html"),
+      chunks: ["signup"],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./search.html"),
+      inject: true,
+      filename: path.resolve(__dirname, "dist/search.html"),
+      chunks: ["search"],
+    }),
+    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ["dist"] }), // re-build될 때 기존의 결과물을 제거해준다
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["**/*.LICENSE.txt"],
+      protectWebpackAssets: false,
+    }), // re-build될 때 기존의 결과물을 제거해준다
   ],
 };
 
